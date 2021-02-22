e096441dee74473a3d76ab1ec8757fba26518ff5
luci <lucia81@protonmail.com>
2021-02-22 17:51:09 -0300

    testes next js

--- .next/server/pages/sobre.js
+++ .next/server/pages/sobre.js
@@ -2949,7 +2949,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
 /* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
 
-var _jsxFileName = "C:\\Users\\Windows\\Desktop\\nextjs\\pages\\sobre.js";
+var _jsxFileName = "C:\\Users\\Windows\\Desktop\\nextjs\\testes-next-js\\pages\\sobre.js";
 
 
 function Sobre() {
