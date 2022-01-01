3102481fba61f467bb5d420d9f8241c782ccc660
LuciLua <luci.lua81@gmail.com>
2022-01-01 18:44:38 -0300

    👋 Footer with my datas... ok sizes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,26 @@
 import styles from './footer.module.scss'
+import Link from 'next/link'
 
 function Footer() {
     return (
         <>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    <h1>
-                        Footer
-                    </h1>
+                    <Link href={"https://luci-lua.tk/"}>
+                        <a>
+                            Portfólio
+                        </a>
+                    </Link>
+                    <Link href={"https://github.com/LuciLua/"}>
+                        <a>
+                            GitHub
+                        </a>
+                    </Link>
+                    <Link href={"https://www.linkedin.com/in/l%C3%BAcia-guelber-837a50185/"}>
+                        <a>
+                            Linkedin
+                        </a>
+                    </Link>
                 </div>
             </div>
         </>
