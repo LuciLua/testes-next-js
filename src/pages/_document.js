12d31b3b6be15268082d9857675bea1699cd573a
LuciLua <luci.lua81@gmail.com>
2022-01-01 18:30:36 -0300

    👋 New visual with background radial-dradient and transparent header

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ export default class myDocument extends Document {
                     <head>
                         <link rel="preconnect" href="https://fonts.googleapis.com" />
                         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@500&display=swap" rel="stylesheet" />
+                        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700;800&family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet"/>
                         <title>ContextAPI</title>
                     </head>
                 </Head>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,20 @@ function Home() {
         <>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    <h1>
-                        Hello, {nome}
-                    </h1>
-                    <p>
-                        Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
-                    </p>
+                    <div className={styles.leftContent}>
+
+                        <h1>
+                            Hello, {nome}
+                        </h1>
+                        <p>
+                            Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos...
+                        </p>
+                        <button>Know</button>
+                    </div>
+                    <div className={styles.rightContent}>
+                        <div className={styles.cubeImg}>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
